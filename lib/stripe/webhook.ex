93374f399774b4f8350c209ec275c45906b4b9c6
Jim Alexander <jalexander@adobe.com>
2024-11-21 08:42:40 -0800

    update to more closely match v2.6.0

--- lib/stripe/webhook.ex
+++ lib/stripe/webhook.ex
@@ -116,7 +116,7 @@ defmodule Stripe.Webhook do
   end
 
   defp compute_signature(payload, secret) do
-    :crypto.mac(:hmac, :sha256, secret, payload)
+    :crypto.hmac(:sha256, secret, payload)
     |> Base.encode16(case: :lower)
   end
 
